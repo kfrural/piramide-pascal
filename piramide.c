46c4b6cf4323edc2361ec2638bde834a36a9a2ef
Karla C. Ferreira <106109272+kfrural@users.noreply.github.com>
2023-05-15 18:50:55 -0300

    Create piramide.c

---
+++ piramide.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+int main ()
+{
+    int i,j;
+int matrix[15][15];
+    for (i=0; i<15; i++) {
+        for (j=0; j<15; j++) {
+            matrix[i][j]=0;
+        }
+    }
+    for (i=0; i<15; i++)
+    {   matrix[i][0]=1;
+    }
+    for(i=1; i<15; i++) {
+        for (j=1; j<15; j++)  {
+            matrix[i][j]=matrix[i-1][j]+matrix[i-1][j-1];
+        }
+    }
+    for (i=0; i<15; i++) {
+        for (j=0; j<15; j++) {
+            if (matrix[i][j]!=0)
+                printf ("%d ",matrix[i][j]);
+        }
+        printf ("\n");
+    }
+    return 0;
+}
